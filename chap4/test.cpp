ecb184c3eff70aba7d0e89422de3f5ee2e12c8c3
MB_home <mb_home@mail.ru>
2024-03-12 23:21:44 +0300

    Refactoring test.cpp file, fixed bug with F grade case

--- chap4/test.cpp
+++ chap4/test.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 int main()
 {
-    string name, last_name;
+    std::string name, surname;
     char grade;
     short age;
-    cout << "What is your first name? ";
-    getline(cin,name);
-    cout << "What is your last name? ";
-    getline(cin,last_name);
-    cout << "What is your grade? ";
-    cin >> grade;
-    grade ++;
-    cout << "What is your age? ";
-    cin >> age;
-    cout << "you are " << name << ' ' << last_name << endl << "your grade is " << grade << endl << "your age is " << age << endl;
-    cout << "sevs2";
+    
+    std::cout << "What is your first name? ";
+    std::getline(std::cin, name);
+    std::cout << "What is your last name? ";
+    std::getline(std::cin, surname);
+    std::cout << "What is your grade? ";
+    std::cin >> grade;
+    if (grade != 'F') grade++;
+    std::cout << "What is your age? ";
+    std::cin >> age;
+
+    // Display
+    std::cout << "You are " << name << ' ' << surname << std::endl;
+    std::cout << "Your grade is " << grade << std::endl; 
+    std::cout << "Your age is " << age << std::endl;
+    
+    std::cout << "sevs2";
 
 }
